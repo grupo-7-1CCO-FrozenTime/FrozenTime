e11261d395715a2f1115b27eeed1b9b78c6006d9
KauanCursino <kauan.souza@sptech.school>
2023-05-25 21:22:40 -0300

    Ajuste de Inserts do BD

--- bdFrozenTime/bancoFrozenTime/FrozenTime.sql
+++ bdFrozenTime/bancoFrozenTime/FrozenTime.sql
@@ -7,12 +7,12 @@ CREATE TABLE Login(
 	senhaFuncionario VARCHAR(20)
 );
 
-INSERT INTO Login (emailFuncionario, senhaFuncionario) VALUES				 
-('anderson@pfizer.com', '024463401$'),
-('angelica@johnson.com', '$2243105@6'),
-('contato@biontech.com', '0#%2084793'),
-('bruna@biontech.com', '560$1044%1'),
-('contato@butantan.com', '28##2964');
+INSERT INTO Login VALUES				 
+(null, 'anderson@pfizer.com', '024463401$'),
+(null, 'angelica@johnson.com', '$2243105@6'),
+(null, 'contato@biontech.com', '0#%2084793'),
+(null, 'bruna@biontech.com', '560$1044%1'),
+(null, 'contato@butantan.com', '28##2964');
 
 SELECT * FROM Login;
 
@@ -29,11 +29,11 @@ complemento VARCHAR(20)
 );
 
 
-INSERT INTO Empresas (nomeEmpresa, cnpjEmpresa, cidade, rua, bairro, numero, complemento) VALUES 
-("Pfizer", "83920374619304", "São Paulo", "Alexandre Dumas", 'São Paulo', 1860, "Prédio"),
-("Butantã", "94738502847294", "São Paulo", "Vital Brasil", 'São Paulo', 1500, "Prédio"),
-("Dismed", "38492840286323", "São Paulo", "Avenida Moça Bonita", 'São Paulo', 559, "Prédio"),
-("J&J", "7483975836599", "São Paulo", "Av. Pres. Juscelino Kubitschek", 'São Paulo', 2041, "Prédio");
+INSERT INTO Empresas VALUES 
+(null, "Pfizer", "83920374619304", "São Paulo", "Alexandre Dumas", 'São Paulo', 1860, "Prédio"),
+(null, "Butantã", "94738502847294", "São Paulo", "Vital Brasil", 'São Paulo', 1500, "Prédio"),
+(null, "Dismed", "38492840286323", "São Paulo", "Avenida Moça Bonita", 'São Paulo', 559, "Prédio"),
+(null, "J&J", "7483975836599", "São Paulo", "Av. Pres. Juscelino Kubitschek", 'São Paulo', 2041, "Prédio");
 
 SELECT * FROM Empresas;
 
@@ -50,8 +50,8 @@ FOREIGN KEY (fkEmpresa) REFERENCES Empresas(idEmpresa),
 FOREIGN KEY (fkLogin) REFERENCES Login(idLogin)
 );
 
-INSERT INTO Funcionarios (nomeFuncionario, emailFuncionario, whatsapp, gestor, fkEmpresa, fkLogin) VALUES
-('Gustavo', 'gustavodessunte@gmail.com', '(11)942961501', 1, 1, 1);
+INSERT INTO Funcionarios VALUES
+(null, 'Gustavo', 'gustavodessunte@gmail.com', '(11)942961501', 1, 1, 1);
 
 SELECT * FROM Funcionarios JOIN Empresas JOIN Clientes;
 
@@ -62,6 +62,12 @@ CREATE TABLE Produtos(
     categoriaProduto VARCHAR(50)
 );
 
+INSERT INTO Produtos VALUES 
+(null, 'Espironolactona', 'Comprimido'),
+(null, 'Ocitocina', 'infusão intravenosa'),
+(null, 'Insulina NPH', 'Injetável ');
+
+SELECT * FROM Produtos;
 
 CREATE TABLE ProdutosEmpresa (
 fkProduto INT,
@@ -70,14 +76,14 @@ fkEmpresa INT,
 FOREIGN KEY(fkEmpresa) REFERENCES Empresas(idEmpresa),
 PRIMARY KEY (fkProduto, fkEmpresa)
 );
+INSERT INTO ProdutoEmpresa VALUES (1, 1),
+								  (1, 2),
+                                  (2, 1),
+                                  (2, 2),
+                                  (2, 3);
 
 
-INSERT INTO Produtos (nomeProduto, categoriaProduto) VALUES 
-('Espironolactona', 'Comprimido'),
-('Ocitocina', 'infusão intravenosa'),
-('Insulina NPH', 'Injetável ');
 
-SELECT * FROM Produtos;
 
 
 CREATE TABLE Rotas(
@@ -88,12 +94,12 @@ CREATE TABLE Rotas(
 	dtChegada DATETIME
 );
 
-INSERT INTO Rotas(localPartida, localDestino, dtSaida, dtChegada) 
-values ('Rua Alexandre Dunas, 1860- Santo Amaro, São Paulo- SP, 04717-904', 'Praça Canáririas, 322- Vila Formosa, São Paulo-SP, 03359-120', '2017-09-08 08:30:05', '2017-09-08 09:30:05'),
-       ('Av.Pres. Juscelino Kubitscheck, 2041- Vila Nova Conceição, São Paulo-SP, 04543-011', 'Av.Paulista, 2125- Bela Vista, São paulo-SP, 01311-000', '2023-03-10 10:00:40', '2023-03-10 12:30:05'),
-       ('Av.Mateo Bei, 3066-Cidade São Mateus, São Paulo-SP, 03949-300', 'Av.Conselheiro Carrão, 2186- Vila Carrão, São Paulo- SP, 03402-002', '2019-04-03 09:50:30', '2019-04-03 11:30:05'),
-       ('R.Martins Fontes, 166-Consolação, são Paulo-SP, 01050-000', 'Av.dos jequitibás, 446-Jabaquara, Mato Grosso do sul-MS, 04321-090', '2022-11-06 11:30:05', '2022-11-07 08:30:05'),
-       ('Av,Lins de Vasconcelos, 1837- Cambuci, São Paulo-SP, 01537-001', 'R.Sete de Abril, 268-República, São Paulo-SP, 01044-000', '2021-10-20 08:00:05', '2022-10-20 09:00:35');
+INSERT INTO Rotas
+values (null, 'Rua Alexandre Dunas, 1860- Santo Amaro, São Paulo- SP, 04717-904', 'Praça Canáririas, 322- Vila Formosa, São Paulo-SP, 03359-120', '2017-09-08 08:30:05', '2017-09-08 09:30:05'),
+       (null, 'Av.Pres. Juscelino Kubitscheck, 2041- Vila Nova Conceição, São Paulo-SP, 04543-011', 'Av.Paulista, 2125- Bela Vista, São paulo-SP, 01311-000', '2023-03-10 10:00:40', '2023-03-10 12:30:05'),
+       (null, 'Av.Mateo Bei, 3066-Cidade São Mateus, São Paulo-SP, 03949-300', 'Av.Conselheiro Carrão, 2186- Vila Carrão, São Paulo- SP, 03402-002', '2019-04-03 09:50:30', '2019-04-03 11:30:05'),
+       (null, 'R.Martins Fontes, 166-Consolação, são Paulo-SP, 01050-000', 'Av.dos jequitibás, 446-Jabaquara, Mato Grosso do sul-MS, 04321-090', '2022-11-06 11:30:05', '2022-11-07 08:30:05'),
+       (null, 'Av,Lins de Vasconcelos, 1837- Cambuci, São Paulo-SP, 01537-001', 'R.Sete de Abril, 268-República, São Paulo-SP, 01044-000', '2021-10-20 08:00:05', '2022-10-20 09:00:35');
 
 SELECT * FROM Rotas;
 
@@ -106,11 +112,11 @@ CREATE TABLE Lotes(
 );
 
 INSERT INTO Lotes VALUES  
-(null, '200', 5, null, null),
-(null, '150', 40, null, null),
-(null, '300', 1, null, null),
-(null, '100', 1.5, null, null),
-(null, '50', 55, null, null);
+(null, 3, 5),
+(null, 2, 3),
+(null, 3, 1),
+(null, 1, 2),
+(null, 3, 4);
 														
 SELECT * FROM Lotes;
 
@@ -124,7 +130,8 @@ INSERT INTO Kits VALUES
 (null, 1),
 (null, 2),
 (null, 3),
-(null, 4);
+(null, 4),
+(null, 5);
 
 create table Refrigeradores (
 idRefrigerador INT PRIMARY KEY AUTO_INCREMENT,
