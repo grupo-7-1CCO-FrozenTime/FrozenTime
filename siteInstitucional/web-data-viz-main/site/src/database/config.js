89f313134f160ed350799515eb66bd39b9c78d32
gustavodessunte <gustavo.bernardes@sptech.school>
2023-06-03 17:12:03 -0300

    alterado o diretorio da pasta Dashboard

--- siteInstitucional/web-data-viz-main/site/src/database/config.js
+++ siteInstitucional/web-data-viz-main/site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "FrozenTime",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "Gustavoluiz060213@",
 };
 
 function executar(instrucao) {
