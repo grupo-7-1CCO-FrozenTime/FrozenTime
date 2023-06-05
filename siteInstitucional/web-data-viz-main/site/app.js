d5be8128f10f0a4896dd30157a9ad6c7aa0dc43f
Isabela Noronha <125585246+Isa14-noronha@users.noreply.github.com>
2023-06-05 13:31:00 -0300

    Api de cadastro de refrigeradores

--- siteInstitucional/web-data-viz-main/site/app.js
+++ siteInstitucional/web-data-viz-main/site/app.js
@@ -12,6 +12,7 @@ var indexRouter = require("./src/routes/index");
 var empresaRouter = require("./src/routes/empresa");
 var loginTabelaRouter = require("./src/routes/loginTabela");
 var funcionarioRouter = require("./src/routes/funcionario");
+var cadastroRefrigerador = require("./src/_routes/cadastroRefrigerador");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +24,7 @@ app.use("/", indexRouter);
 app.use("/empresa", empresaRouter);
 app.use("/loginTabela", loginTabelaRouter);
 app.use("/funcionario", funcionarioRouter);
+app.use("/cadastroRefrigerador", cadastroRefrigerador);
 
 app.listen(PORTA, function () {
     console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA} \n
