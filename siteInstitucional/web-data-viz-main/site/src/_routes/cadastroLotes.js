b51ae0ffbd66d47f8d830d5005f30cdbd29a3f06
Isabela Noronha <125585246+Isa14-noronha@users.noreply.github.com>
2023-06-05 15:32:19 -0300

    Adicao da API de cadastro de lotes/rotas

---
+++ siteInstitucional/web-data-viz-main/site/src/_routes/cadastroLotes.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+var cadastroLotesController = require("../_controllers/cadastroLotesController");
+
+router.get("/", function (req, res) {
+    cadastroLotesController.testar(req, res);
+});
+
+//Recebendo os dados do html e direcionando para a função cadastrar de cadastroLotesController.js
+router.post("/cadastrarRota", function (req, res) {
+    cadastroLotesController.cadastrar(req, res);
+})
+
+router.post("/cadastrarLote", function (req, res) {
+    cadastroLotesController.cadastrar(req, res);
+})
+
+
+
+module.exports = router;
